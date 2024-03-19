ffff12a57ae0a0e459b062de99509f91de48621c
Leandro Dias <lvd@certi.org.br>
2024-03-19 00:07:01 -0300

    inicia projeto

---
+++ src/main/java/com/api/conectaProfessorAluno/AgendaDeAulasApplication.java
@@ -0,0 +1,13 @@
+package com.api.conectaProfessorAluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaDeAulasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaDeAulasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/conectaProfessorAluno/ConectaProfessorAlunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.conectaProfessorAluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaProfessorAlunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
